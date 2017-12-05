de1561f76b6f387f79cc879b16df74d0f45eeb5a
Phelipe Rocha <phelipe10_rocha@hotmail.com>
2017-12-04 23:44:27 -0200

    Incluindo as demais variáveis públicas ao DebugPanel

--- Assets/demo/scripts/DebugPanel.cs
+++ Assets/demo/scripts/DebugPanel.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class DebugPanel : MonoBehaviour {
 
-    public Text gravidade, velocidadeMov;
+    public Text gravidade, velocidadeMov, alturaPulo, multiplicadorCorrida, velocidadeRolar, tempoRolar;
 
     DemoScene _player;
 
@@ -25,9 +25,9 @@ public class DebugPanel : MonoBehaviour {
     {
         if (gravidade.text != "") _player.gravity = float.Parse(gravidade.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
         if (velocidadeMov.text != "") _player.runSpeed = float.Parse(velocidadeMov.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
-
-
-
-
+        if (alturaPulo.text != "") _player.jumpHeight = float.Parse(alturaPulo.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+        if (multiplicadorCorrida.text != "") _player.runMultiplier = float.Parse(multiplicadorCorrida.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+        if (velocidadeRolar.text != "") _player.rollingSpeed = float.Parse(velocidadeRolar.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+        if (tempoRolar.text != "") _player.rollingTime = float.Parse(tempoRolar.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
     }
 }
